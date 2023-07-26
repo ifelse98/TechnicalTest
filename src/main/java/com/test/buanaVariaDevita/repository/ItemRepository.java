bae40980d3bf071024b249ec451f3a48f05cfe2a
unknown <nirwanrhamdani70@gmail.com>
2023-07-26 16:52:42 +0700

    Hapus code yang tidak perlu

--- src/main/java/com/test/buanaVariaDevita/repository/ItemRepository.java
+++ src/main/java/com/test/buanaVariaDevita/repository/ItemRepository.java
@@ -1,13 +1,10 @@
 package com.test.buanaVariaDevita.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.test.buanaVariaDevita.model.Item;
 
 public interface ItemRepository extends JpaRepository<Item, String> {
-	
-	List<Item> findAllById(String itemId);
 
 }
